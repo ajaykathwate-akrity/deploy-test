c92fde7f57e0ebc37376007a98c6ea4aa9d9b85b
ajaykathwate-akrity <ajay.kathwate@akrity.com>
2024-07-17 19:25:12 +0530

    workflow changes 02

--- main.js
+++ main.js
@@ -18,9 +18,6 @@ document.querySelector('#app').innerHTML = `
     <p class="read-the-docs">
       Click on the Vite logo to learn more
       Click on the Vite logo to learn more
-      Click on the Vite logo to learn more
-      Click on the Vite logo to learn more
-      Click on the Vite logo to 
     </p>
   </div>
 `
